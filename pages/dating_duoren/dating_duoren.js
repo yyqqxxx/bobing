1b35fceecb62085407605db0a8777871cfa68f29
yyqqxxx <642267211@qq.com>
2021-10-09 14:57:06 +0800

    7

--- pages/dating_duoren/dating_duoren.js
+++ pages/dating_duoren/dating_duoren.js
@@ -4,7 +4,6 @@ var roomID = ""
 var num = ""
 var p=1
 var rank = ""
-var score = "" //记录积分
 
 Page({
     /**
@@ -15,7 +14,6 @@ Page({
       statu:0, //状态：0创建房间界面，1未开始，2投掷中, 3出结果
       resultList:[],
       rank:"",    //单机情况玩家判定结果
-      score:"",   //记录积分
       rank1:"",    //玩家1判定的结果
       rank2:"",    //玩家2判定的结果
       rank3:"",    //玩家3判定的结果
@@ -137,21 +135,11 @@ Page({
     result(){
       var list=[];
       var listsrc=[];
-      var one=0;  //统计投掷结果
-      var two=0;
-      var three=0;
-      var four=0;
-      var five=0;
-      var six=0;
+      var countList=[0,0,0,0,0,0,0];
       for(var i=0;i<6;i++){
           var t=this.random(1,6);
           list.push(t);
-          if(t==1)one++;
-          if(t==2)two++;
-          if(t==3)three++;
-          if(t==4)four++;
-          if(t==5)five++;
-          if(t==6)six++;
+         countList[t]++;
         }
       this.setData({
         resultList:list
@@ -163,90 +151,88 @@ Page({
       this.setData({
         dicelist:listsrc,
       })
-      var flag=0;
-      if(one==1&&two==1&&three==1&&four==1&&five==1&&six==1){
+      var flag = 0;
+      if ( countList[1] == 1 &&  countList[2] == 1 &&  countList[3] == 1 &&  countList[4] == 1 &&  countList[5]== 1 &&  countList[6] == 1) {
         this.setData({
-          rank:"对堂！",
-          score:"+300"
+          rank: "对堂！",
         })
-        flag=1
-      }else{
-        if(four==1){
-        this.setData({
-          rank:"一秀！",
-          score:"+10"
-        })
-        flag=1
-      }
-      if(four==2){
-        this.setData({
-          rank:"二举！",
-          score:"+50"
-        })
-        flag=1
-      }
-      if(four==3){
-        this.setData({
-          rank:"三红！",
-          score:"+200"
-        })
-        flag=1
-      }
-      if(four==4){
-        if(one==2){
+        flag = 1
+      } else {
+        if ( countList[1] == 6){
           this.setData({
-            rank:"状元插金花！",
-            score:"+1000"
+            rank:"遍地锦！",
           })
-          flag=1
         }
-        else{
+        if ( countList[4]== 1) {
           this.setData({
-            rank:"状元！",
-            score:"+500"
+            rank: "一秀！",
           })
-          flag=1
+          flag = 1
+        }
+        if ( countList[4]== 2) {
+          this.setData({
+            rank: "二举！",
+          })
+          flag = 1
+        }
+        if ( countList[3] == 3) {
+          this.setData({
+            rank: "三红！",
+          })
+          flag = 1
+        }
+        if ( countList[4] == 5) {
+          this.setData({
+            rank: "五王！",
+          })
+          flag = 1
+        }
+        if ( countList[4] == 4) {
+          if ( countList[1] == 2) {
+            this.setData({
+              rank: "状元插金花！",
+            })
+            flag = 1
+          } else {
+            this.setData({
+              rank: "状元！",
+            })
+            flag = 1
+          }
+        }
+        if ( countList[4] == 6) {
+          this.setData({
+            rank: "六杯红！",
+          })
+          flag = 1
+        }
+        if ( countList[1] == 4 ||  countList[2]== 4 || countList[3] == 4  ||  countList[5] == 4 ||  countList[6] == 4) {
+          this.setData({
+            rank: "四进！",
+          })
+          flag = 1
+        }
+        if ( countList[1] == 5 || countList[2] == 5 ||  countList[3] == 5  ||  countList[5] == 5 || countList[6] == 5) {
+          this.setData({
+            rank: "五子登科！",
+          })
+          flag = 1
+        }
+        if ( countList[2] == 6 ||  countList[3] == 6  || countList[5] == 6 ||  countList[6]== 6) {
+          this.setData({
+            rank: "六杯黑！",
+          })
+          flag = 1
         }
       }
-      if(four==6){
+      if (flag == 0) {
         this.setData({
-          rank:"六杯红！",
-          score:"+500"
+          rank: "倒霉蛋！",
         })
-        flag=1
       }
-      if(three==4){
-        this.setData({
-          rank:"四进！",
-          score:"+100"
-        })
-        flag=1
-      }
-      if(three==5){
-        this.setData({
-          rank:"五王！",
-          score:"+500"
-        })
-        flag=1
-      }
-      if(six==6){
-        this.setData({
-          rank:"六杯黑！",
-          score:"+500"
-        })
-        flag=1
-      }
-    }
-    if(flag==0){
-      this.setData({
-        rank:"可惜",
-        score:"+0"
-      })
-    }
       // console.log(this.data.resultList)
       console.log("输出投掷结果:",this.data.rank)
       rank=this.data.rank
-      score=this.data.score
     },
 
     click3(){
@@ -260,7 +246,6 @@ Page({
       p:2,
       statu:1,
       rank1:rank,
-      score1:score
     })
   },
 
@@ -269,7 +254,6 @@ Page({
       p:3,
       statu:1,
       rank2:rank,
-      score2:score
     })
   },
 
@@ -278,7 +262,6 @@ Page({
       p:4,
       statu:1,
       rank3:rank,
-      score3:score
     })
   },
 
@@ -289,22 +272,20 @@ Page({
     if(num=='2人'){
       this.setData({
         rank2:rank,
-        score2:score
       })
     }
     if(num=='3人'){
       this.setData({
         rank3:rank,
-        score3:score
       })
     }
 
     if(num=='4人'){
       this.setData({
         rank4:rank,
-        score4:score
       })
     }
     console.log("排名完成")
   },
-})
+}
+)
